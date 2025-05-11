8600eb3714e6c988a840006ffcf540a2b2447e07
Akilapa Godspower <akilapanunayan@gmail.com>
2025-05-11 21:08:35 +0100

    api integration

--- actions/get_exchange.js
+++ actions/get_exchange.js
@@ -1,79 +1,88 @@
 "use server";
+
 import axios from "axios";
 
-export const getCryptoList = async () => {
-  try {
-    const { data } = await axios.get(
-      `${process.env.NEXT_PUBLIC_COINMARKETCAP_BASE_URL}/v1/cryptocurrency/map`,
-      {
-        headers: {
-          "X-CMC_PRO_API_KEY": process.env.COINMARKETCAP_API_KEY,
-        },
-      }
-    );
+import cctx, {
+  version,
+  exchanges,
+  alpaca,
+  apex,
+  binance,
+  bingx,
+  kraken,
+} from "ccxt";
 
-    // ! IF SUCCESSFULLY RETRIEVED DATA
-    if (data) {
-      return { res: data };
-    }
-  } catch (error) {
-    // console.log(error);
-    return {
-      message: "Network Error, Please Try Again",
-      error: error,
-    };
-  }
-};
+// export const getCryptoList = async () => {
+//   try {
+//     const { data } = await axios.get(
+//       `${process.env.NEXT_PUBLIC_COINMARKETCAP_BASE_URL}/v1/cryptocurrency/map`,
+//       {
+//         headers: {
+//           "X-CMC_PRO_API_KEY": process.env.COINMARKETCAP_API_KEY,
+//         },
+//       }
+//     );
 
-export const getExchangeList = async () => {
-  try {
-    const { data } = await axios.get(
-      `${process.env.NEXT_PUBLIC_COINMARKETCAP_BASE_URL}/v1/exchange/map`,
-      {
-        params: {
-          limit: 100,
-          crypto_id: 1,
-        },
-        headers: {
-          "X-CMC_PRO_API_KEY": process.env.COINMARKETCAP_API_KEY,
-        },
-      }
-    );
+//     // ! IF SUCCESSFULLY RETRIEVED DATA
+//     if (data) {
+//       return { res: data };
+//     }
+//   } catch (error) {
+//     // console.log(error);
+//     return {
+//       message: "Network Error, Please Try Again",
+//       error: error,
+//     };
+//   }
+// };
 
-    // ! IF SUCCESSFULLY RETRIEVED DATA
-    if (data) {
-      return { res: data };
-    }
-  } catch (error) {
-    // console.log(error);
-    return {
-      message: "Network Error, Please Try Again",
-      error: error,
-    };
-  }
-};
+// export const getExchangeList = async () => {
+//   try {
+//     const { data } = await axios.get(
+//       `${process.env.NEXT_PUBLIC_COINMARKETCAP_BASE_URL}/v1/exchange/map`,
+//       {
+//         params: {
+//           limit: 100,
+//           crypto_id: 1,
+//         },
+//         headers: {
+//           "X-CMC_PRO_API_KEY": process.env.COINMARKETCAP_API_KEY,
+//         },
+//       }
+//     );
+
+//     // ! IF SUCCESSFULLY RETRIEVED DATA
+//     if (data) {
+//       return { res: data };
+//     }
+//   } catch (error) {
+//     // console.log(error);
+//     return {
+//       message: "Network Error, Please Try Again",
+//       error: error,
+//     };
+//   }
+// };
 
 export const getExchanges = async () => {
   try {
-    const { data } = await axios.get(
-      `${process.env.NEXT_PUBLIC_COINMARKETCAP_BASE_URL}/v1/exchange/info`,
-      {
-        params: {
-          limit: 100,
-          crypto_id: 1,
-        },
-        headers: {
-          "X-CMC_PRO_API_KEY": process.env.COINMARKETCAP_API_KEY,
-        },
-      }
-    );
+    const exchange = new binance({ enableRateLimit: true });
+    const ticker = await exchange.fetch_ticker("BTC/USDT");
+    console.log(ticker);
 
-    // ! IF SUCCESSFULLY RETRIEVED DATA
-    if (data) {
-      return { res: data };
-    }
+    // const ticker = await binance().publicGetTicker24hr();
+
+    // console.log(new cctx.binance());
+
+    // if (version && exchanges) {
+    //   return {
+    //     res: ticker,
+    //     // exchange: exchange,
+    //     exchanges: Object.keys(exchanges),
+    //   };
+    // }
   } catch (error) {
-    // console.log(error);
+    console.log(error);
     return {
       message: "Network Error, Please Try Again",
       error: error,
--- components/sections/Exchanges.jsx
+++ components/sections/Exchanges.jsx
@@ -3,16 +3,13 @@
 import { useEffect, useState } from "react";
 
 // SERVER ACTIONS
-import { getExchangeList } from "@/actions/get_exchange";
+import { getExchanges } from "@/actions/get_exchange";
 
 const Exchanges = () => {
   const [exchangeList, setExchangeList] = useState([]);
   useEffect(() => {
-    getExchangeList().then((response) => {
-      // console.log(response);
-      response.res.data.map((element) =>
-        setExchangeList((prev) => [...prev, element.id])
-      );
+    getExchanges().then((response) => {
+      console.log(response);
       // console.log(exchangeList);
     });
   }, []);
