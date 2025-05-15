a82229edc5dfab2850925d43e1baad58bb062b13
Akilapa Godspower <akilapanunayan@gmail.com>
2025-05-15 23:37:29 +0100

    exchanges table

--- constants/my_exchange_data.js
+++ constants/my_exchange_data.js
@@ -12,101 +12,101 @@ const data = [
     tickers: {
       "XRP/USDT": {
         symbol: "XRP/USDT",
-        last: 2.3, // Price of XRP in USDT
-        quoteVolume: 1000000, // 24h volume in USD
-        high: 3, // 24h high price in USD
-        low: 2, // 24h low price in USD
+        last: 2.703, // Price of XRP in USDT
+        quoteVolume: 51603352014.8958, // 24h volume in USDT
+        high: 2.8548, // 24h high price in USDT
+        low: 2.6584, // 24h low price in USDT
       },
       "BTC/USDT": {
         symbol: "BTC/USDT",
-        last: 123456.0, // Price of BTC in USDT
-        quoteVolume: 1000000, // 24h volume in USD
-        high: 134567, // 24h high price in USD
-        low: 100000, // 24h low price in USD
+        last: 114118.8, // Price of BTC in USDT
+        quoteVolume: 195873368828.85, // 24h volume in USDT
+        high: 114611.97, // 24h high price in USDT
+        low: 111521.37, // 24h low price in USDT
       },
       "ETH/USDT": {
         symbol: "ETH/USDT",
-        last: 2.3, // Price of ETH in USDT
-        quoteVolume: 1000000, // 24h volume in USD
-        high: 3, // 24h high price in USD
-        low: 2, // 24h low price in USD
+        last: 2836.49, // Price of ETH in USDT
+        quoteVolume: 215749455372.1, // 24h volume in USDT
+        high: 2938.2, // 24h high price in USDT
+        low: 2748.39, // 24h low price in USDT
       },
       "BNB/USDT": {
         symbol: "BNB/USDT",
-        last: 2.3, // Price of BNB in USDT
-        quoteVolume: 1000000, // 24h volume in USD
-        high: 3, // 24h high price in USD
-        low: 2, // 24h low price in USD
+        last: 725.24, // Price of BNB in USDT
+        quoteVolume: 22269679769.438, // 24h volume in USDT
+        high: 733.71, // 24h high price in USDT
+        low: 712.941, // 24h low price in USDT
       },
       "SOL/USDT": {
         symbol: "SOL/USDT",
-        last: 2.3, // Price of SOL in USDT
-        quoteVolume: 1000000, // 24h volume in USD
-        high: 3, // 24h high price in USD
-        low: 2, // 24h low price in USD
+        last: 190.261, // Price of SOL in USDT
+        quoteVolume: 66376945178.7612, // 24h volume in USDT
+        high: 199.7296, // 24h high price in USDT
+        low: 186.7039, // 24h low price in USDT
       },
       "DOGE/USDT": {
         symbol: "DOGE/USDT",
-        last: 2.3, // Price of DOGE in USDT
-        quoteVolume: 1000000, // 24h volume in USD
-        high: 3, // 24h high price in USD
-        low: 2, // 24h low price in USD
+        last: 0.2216, // Price of DOGE in USDT
+        quoteVolume: 343220115.1731, // 24h volume in USDT
+        high: 0.23676, // 24h high price in USDT
+        low: 0.21604, // 24h low price in USDT
       },
       "LTC/USDT": {
         symbol: "LTC/USDT",
-        last: 2.3, // Price of LTC in USDT
-        quoteVolume: 1000000, // 24h volume in USD
-        high: 3, // 24h high price in USD
-        low: 2, // 24h low price in USD
+        last: 111.93, // Price of LTC in USDT
+        quoteVolume: 6716435703.0, // 24h volume in USDT
+        high: 114.12, // 24h high price in USDT
+        low: 107.04, // 24h low price in USDT
       },
       "TRX/USDT": {
         symbol: "TRX/USDT",
-        last: 2.3, // Price of TRX in USDT
-        quoteVolume: 1000000, // 24h volume in USD
-        high: 3, // 24h high price in USD
-        low: 2, // 24h low price in USD
+        last: 0.274, // Price of TRX in USDT
+        quoteVolume: 183785147.39429, // 24h volume in USDT
+        high: 0.2771, // 24h high price in USDT
+        low: 0.2673, // 24h low price in USDT
       },
       "ADA/USDT": {
         symbol: "ADA/USDT",
-        last: 2.3, // Price of ADA in USDT
-        quoteVolume: 1000000, // 24h volume in USD
-        high: 3, // 24h high price in USD
-        low: 2, // 24h low price in USD
+        last: 0.7679, // Price of ADA in USDT
+        quoteVolume: 136071353.68051, // 24h volume in USDT
+        high: 0.8102, // 24h high price in USDT
+        low: 0.7445, // 24h low price in USDT
       },
       "DOT/USDT": {
         symbol: "DOT/USDT",
-        last: 2.3, // Price of DOT in USDT
-        quoteVolume: 1000000, // 24h volume in USD
-        high: 3, // 24h high price in USD
-        low: 2, // 24h low price in USD
+        last: 4.81, // Price of DOT in USDT
+        quoteVolume: 35484988.42, // 24h volume in USDT
+        high: 5.028, // 24h high price in USDT
+        low: 4.685, // 24h low price in USDT
       },
       "LINK/USDT": {
         symbol: "LINK/USDT",
-        last: 2.3, // Price of LINK in USDT
-        quoteVolume: 1000000, // 24h volume in USD
-        high: 3, // 24h high price in USD
-        low: 2, // 24h low price in USD
+        last: 16.09, // Price of LINK in USDT
+        quoteVolume: 72261196.59, // 24h volume in USDT
+        high: 17.21, // 24h high price in USDT
+        low: 15.79, // 24h low price in USDT
       },
       "UNI/USDT": {
         symbol: "UNI/USDT",
-        last: 2.3, // Price of UNI in USDT
-        quoteVolume: 1000000, // 24h volume in USD
-        high: 3, // 24h high price in USD
-        low: 2, // 24h low price in USD
+        last: 6.241, // Price of UNI in USDT
+        quoteVolume: 44870274.4975, // 24h volume in USDT
+        high: 6.783, // 24h high price in USDT
+        low: 6.131, // 24h low price in USDT
       },
       "AAVE/USDT": {
         symbol: "AAVE/USDT",
-        last: 2.3, // Price of AAVE in USDT
-        quoteVolume: 1000000, // 24h volume in USD
-        high: 3, // 24h high price in USD
-        low: 2, // 24h low price in USD
+        last: 224.8, // Price of AAVE in USDT
+        quoteVolume: 42334456.532, // 24h volume in USDT
+        high: 235.32, // 24h high price in USDT
+        low: 216.8, // 24h low price in USDT
       },
       "AVAX/USDT": {
         symbol: "AVAX/USDT",
-        last: 2.3, // Price of AVAX in USDT
-        quoteVolume: 1000000, // 24h volume in USD
-        high: 3, // 24h high price in USD
-        low: 2, // 24h low price in USD
+        last: 23.6, // Price of AVAX in USDT
+        quoteVolume: 85880169.2369, // 24h volume in USDT
+        high: 25.13, // 24h high price in USDT
+        low: 23.0, // 24h low price in USDT
       },
     },
   },
